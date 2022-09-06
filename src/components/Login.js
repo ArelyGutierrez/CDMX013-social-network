185ce0d7510f04f7b415cd6d302ac4beefb5df71
Bere Frias <valkiriadehierro@yahoo.com>
2022-09-06 18:51:29 -0500

    preLogin

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,32 +2,50 @@ import { onNavigate } from '../main.js';
 
 export const Login = () => {
   const div = document.createElement('div');
-  const title = document.createElement('h2');
-  const buttonLogin = document.createElement('button');
-  const buttonBack = document.createElement('button');
-  const inputEmail = document.createElement('input');
-  const inputPass = document.createElement('input');
+  const titleLogin = document.createElement('h2');
+  const buttonEmail = document.createElement('button');
+  const buttonPass = document.createElement('button');
+  const buttonGithub = document.createElement('button');
+  const buttonLoginStart = document.createElement('button');
+  const paragraphLogin = document.createElement('p');
+  const inputMail = document.createElement('input');
+  const inputPasw = document.createElement('input');
+  const paragraphReg = document.createElement('p');
+  const buttonRegisterReturn = document.createElement('href');
 
-  buttonLogin.textContent = 'Entrar';
-  buttonBack.textContent = 'Regresar';
-  title.textContent = 'Inicia Sesion';
+  titleLogin.textContent = 'Iniciar Sesión';
+  buttonEmail.textContent = 'Iniciar Sesión con Google';
+  buttonPass.textContent = 'Iniciar Sesión con Twitter';
+  buttonGithub.textContent = 'Iniciar Sesión con GitHub';
+  buttonLoginStart.textContent = 'Iniciar sesión';
+  paragraphLogin.textContent = '____________o____________';
+  inputMail.textContent = 'Email';
+  inputPasw.textContent = 'Password';
+  paragraphReg.textContent = '¿No tienes una cuenta?';
+  buttonRegisterReturn.textContent = 'Registrate';
 
-  div.className = 'divLogin';
-  title.className = 'titleLogin';
-  buttonLogin.className = 'buttonLogin';
-  buttonBack.className = 'buttonBack';
-  inputEmail.className = 'inputEmail';
-  inputPass.className = 'inputPass';
+  div.className = 'divWelcome';
+  titleLogin.className = 'titleLogin';
+  buttonLoginStart.className = 'buttonLogin';
+  buttonEmail.className = 'buttonEmail';
+  buttonPass.className = 'buttonPass';
+  buttonGithub.className = 'buttonGithub';
+  buttonLoginStart.className = 'buttonLogin';
+  paragraphLogin.className = 'paragraphLogin';
+  inputMail.className = 'inputMail';
+  inputPasw.className = 'inputPasw';
+  paragraphReg.className = 'paragraphReg';
+  buttonRegisterReturn.className = 'buttonRegisterReturn';
 
-  buttonLogin.addEventListener('click', () => {
-    console.log(inputEmail.value);
+  buttonLoginStart.addEventListener('click', () => {
+    console.log(buttonEmail.value);
     onNavigate('/wall');
   });
-  buttonBack.addEventListener('click', () => {
-    onNavigate('/');
+  buttonRegisterReturn.addEventListener('click', () => {
+    onNavigate('/register');
   });
 
-  div.append(title, inputEmail, inputPass, buttonLogin, buttonBack);
+  div.append(titleLogin, buttonEmail, buttonPass, buttonGithub, paragraphLogin, inputMail, inputPasw, buttonLoginStart, paragraphReg, buttonRegisterReturn);
 
   return div;
 };
