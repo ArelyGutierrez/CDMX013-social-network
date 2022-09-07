19fbf27c1e8c100a1084865e29c7a9559682fc37
Andrea Barajas <108833574+AndreBarajas@users.noreply.github.com>
2022-09-06 23:07:16 -0500

    agregar la app del config a la autenticacion

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,5 +1,5 @@
-/* eslint-disable import/no-unresolved */
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.9.3/firebase-auth-compat.js';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.9.3/firebase-auth.js';
+import { app } from './config.js';
 
 const auth = getAuth();
 export const newUser = (email, password) => createUserWithEmailAndPassword(auth, email, password);
